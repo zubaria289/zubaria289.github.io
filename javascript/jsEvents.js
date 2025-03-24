000195ac214a89790079abe980ada47203eb3cdd
zubaria289 <28938244@students.lincoln.ac.uk>
2025-03-24 12:58:45 +0000

    rgb random background and working on the encryption shift

--- javascript/jsEvents.js
+++ javascript/jsEvents.js
@@ -1,23 +1,39 @@
-/*
+
 // returns a random colour as string "rbg(x,x,x)"
 function randomRGB() {
     let rgbArray = [255, 255, 255];
     let rgb = rgbArray.map(x => x * Math.random());
-    return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+    return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'; //changing them all to 0 makes it greyscale
 }
 // change page background to a random colour on every click
 let body = document.getElementsByTagName("body")[0];
 body.addEventListener("click", function () {
+    // alert("test");
     body.style.background = randomRGB();
 });
-*/
+
 
 //const image = document.querySelectorAll(".box");
-const output = document.querySelectorAll("outputText");
-output.addEventListener("click", encrypted);
+function encrypt() {
+    return "test";
 
+};
 
-function encrypted(event) {
-    alert("hmm");
+const output = document.getElementById("submitText");
+output.addEventListener("click", function () {
+
+    var input = document.getElementById("inputText").value;
+    var result = " ";
+    var out = " ";
+    for  
+
+    // let strEncrypted = encrypt();
+    alert(input);
+
 }
+
+);
+
+
+
 
